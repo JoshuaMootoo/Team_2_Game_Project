9c3852a27fae6b4f47fa7ef7fe2293c4a502bab0
Joshua Mootoo <joshuamoo2@hotmail.com>
2021-02-26 09:31:24 +0000

    Added Wall Jump, Transform Mechanic and Camera Follow
    Wall Jump - need more work done, has a few bugs.
    
    Transform Mechanic - has Temp Variables for the cube and sphere, ready for animation to be added.
    
    Camera Follow - Camera now follows Player.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform playerTransform;
+    public Vector3 offsetPos;
+    public float followSpeed;
+    public float xMin;
+    Vector3 velocity = Vector3.zero;
+
+    private void Awake()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    private void Start()
+    {
+        offsetPos = new Vector3(0, 2, -10);
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 targetPos = playerTransform.position + offsetPos;
+        Vector3 clampedPos = new Vector3(Mathf.Clamp(targetPos.x, xMin, float.MaxValue), targetPos.y, targetPos.z);
+        Vector3 smoothPos = Vector3.SmoothDamp(transform.position, clampedPos, ref velocity, followSpeed * Time.fixedDeltaTime);
+
+        this.transform.position = smoothPos;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,43 +4,141 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    //  Movement Controls
-    Rigidbody2D rb2d;
+    //  Player Variables
+    private Rigidbody2D rb2d;
+    private float gravityStore;
 
-    public float moveSpeed = 10;
+    //  Movement Variables
+    [Header("Player Movement Variables")]
+    public float playerSpeed = 10.0f;
+    private float moveButton;
+    private bool faceingLeft;
 
-    public float jumpForce = 10;
+    //  Jump Variables
+    [Header("Player Jump Variables")]
+    public float jumpForce = 15.0f;
     public Transform groundCheck;
     public LayerMask whatIsGround;
+    private bool jumpButton, grounded;
+
+    //  Wall Jump Variables
+    [Header("Player Wall Jump Variables")]
+    public Transform wallCheck;
+    private bool isWall, isGrabbing;
+
+    //  Transform Variables
+    [Header("Transform Variables")]
+    public GameObject _cube;
+    public GameObject _sphere; // Test Variables to show the Mechanic can work
+    public bool isBall = false;
 
 
-
-    private void Start()
+    private void Awake()
     {
         rb2d = this.transform.GetComponent<Rigidbody2D>();
         groundCheck = this.transform.GetChild(0).GetComponent<Transform>();
+        wallCheck = this.transform.GetChild(1).GetComponent<Transform>();
+    }
+
+    private void Start()
+    {
+        gravityStore = rb2d.gravityScale;
     }
 
     private void FixedUpdate()
     {
         PlayerMovement();
         PlayerJump();
+        PlayerTransform();
     }
-    
+
     void PlayerMovement()
     {
         //  Player Movement
-        float move = Input.GetAxis("Horizontal") * moveSpeed;
+        moveButton = Input.GetAxis("Horizontal") * playerSpeed;
 
-        if (move < 0) rb2d.velocity = new Vector3(move, rb2d.velocity.y);
-        if (move > 0) rb2d.velocity = new Vector3(move, rb2d.velocity.y);
+        if (moveButton > 0)
+        {
+            rb2d.velocity = new Vector2(moveButton, rb2d.velocity.y);
+            faceingLeft = false;
+        }
+        if (moveButton < 0)
+        {
+            rb2d.velocity = new Vector2(moveButton, rb2d.velocity.y);
+            faceingLeft = true;
+        }
+
+        // Player Direction
+        if (faceingLeft) this.transform.localScale = new Vector3(-1, 1, 1);
+        else this.transform.localScale = new Vector3(1, 1, 1);
     }
+
     void PlayerJump()
     {
         //  Player Jump
-        bool jump = Input.GetButton("Jump");
-        bool grounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, whatIsGround);
+        jumpButton = Input.GetButton("Jump");
+        grounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, whatIsGround);
 
-        if (jump && grounded) rb2d.AddForce(Vector3.up * jumpForce);
-    } 
+        if (jumpButton && grounded) rb2d.AddForce(Vector2.up * jumpForce * 10);
+
+        //  Wall Jump
+        isWall = Physics2D.OverlapCircle(wallCheck.position, 0.2f, whatIsGround);
+
+        isGrabbing = false;
+
+        if (!grounded && isWall)
+        {
+            isGrabbing = true;
+        }
+
+        if (isGrabbing)
+        {
+            rb2d.gravityScale = 0;
+            if (jumpButton)
+            {
+                rb2d.gravityScale = gravityStore;
+                if (faceingLeft)
+                {
+                    rb2d.velocity = new Vector2(jumpForce / 2, jumpForce / 2);
+                    faceingLeft = !faceingLeft;
+                }
+                else
+                {
+                    rb2d.velocity = new Vector2(-jumpForce / 2, jumpForce / 2);
+                    faceingLeft = !faceingLeft;
+                }
+                isGrabbing = false;
+            }
+
+        }
+        else
+        {
+            rb2d.gravityScale = gravityStore;
+        }
+    }
+
+    void PlayerTransform()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift)) isBall = !isBall;
+
+        if (isBall)
+        {
+            _cube.SetActive(false);
+            _sphere.SetActive(true);
+        }
+        else
+        {
+            _cube.SetActive(true);
+            _sphere.SetActive(false);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawSphere(groundCheck.position, 0.2f);
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawSphere(wallCheck.position, 0.2f);
+    }
 }
