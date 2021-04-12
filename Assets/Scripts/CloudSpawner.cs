400fe78a986b14946472d4d650aae0bcd5e347f0
JimmyRammelkamp <jimi334455@gmail.com>
2021-04-12 17:16:22 +0100

    Cloud Spawner

---
+++ Assets/Scripts/CloudSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudSpawner : MonoBehaviour
+{
+    public GameObject[] cloudPrefabs;
+    public float spawnRangeY = 0.5f ;
+    public float spawnPosX = 20;
+    public float startDelay = 2;
+    public float spawnInterval = 1.5f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    void SpawnRandomAnimal()
+    {
+        int cloudIndex = Random.Range(0, cloudPrefabs.Length);
+        Vector2 spawnPos = new Vector2(spawnPosX + transform.position.x,transform.position.y + Random.Range(0, spawnRangeY));
+
+        Instantiate(cloudPrefabs[cloudIndex], spawnPos, cloudPrefabs[cloudIndex].transform.rotation);
+    }
+}
